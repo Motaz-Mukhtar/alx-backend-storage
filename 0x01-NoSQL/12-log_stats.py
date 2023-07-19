5274758ebd1378e087c4ffc9f1b2d20d052c6bc5
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-18 18:55:08 -0700

    Update 12-log_stats.py file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,13 +9,17 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     nginx_collection = client.logs.nginx
 
-    print(f"{nginx_collection.count_document()} logs")
+    result = nginx_collection.find()
+    doc_list = [doc for i in result]
+
+    print(f"{len(doc_list)} logs")
     print(f"""
     Methods:
-        method GET: {nginx_collection.count_document({'method': 'GET'})}
-        method POST: {nginx_collection.count_document({'method': 'POST'})}
-        method PUT: {nginx_collection.count_document({'method': 'PUT'})}
-        method PATCH: {nginx_collection.count_document({'method': 'PATCH'})}
-        method DELETE: {nginx_collection.count_document({'method': 'DELETE'})}
-    {nginx_collection.count_document({'path': '/status'})} status check
+        method GET: {nginx_collection.count_documents({'method': 'GET'})}
+        method POST: {nginx_collection.count_documents({'method': 'POST'})}
+        method PUT: {nginx_collection.count_documents({'method': 'PUT'})}
+        method PATCH: {nginx_collection.count_documents({'method': 'PATCH'})}
+        method DELETE: {nginx_collection.count_documents({'method': 'DELETE'})}
+    {nginx_collection.count_documents({'path': '/status'})} status check
     """)
+    client.close()
