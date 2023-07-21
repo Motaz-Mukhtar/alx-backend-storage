09ec1a86c17218e1caa8da71df8921256a640087
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-21 16:40:26 -0700

    Add decorator @count_calls.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,24 @@
 """
 import uuid
 import redis
+from functools import wraps
 from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+        Increments the count for that key every
+        time the method is called and returns
+        the value returned by the original method.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+            Incerement.
+        """
+        self._redis.incer(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -18,6 +35,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
             Generate a random key store the input
@@ -28,7 +46,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]=None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
              take a key string argument and an Optional
              Callable argument named fn, and convert
@@ -38,9 +57,8 @@ class Cache:
 
         if not data:
             return None
-        
+
         return fn(data)
-        
 
     def get_str(self, key: str) -> str:
         """
@@ -53,3 +71,15 @@ class Cache:
             Convert the data to integer.
         """
         return self.get(key, int)
+
+
+
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
+
