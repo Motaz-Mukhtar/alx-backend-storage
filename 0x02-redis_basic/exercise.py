dd78e0ef54b5e05067604d1929461627b9f911ec
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-21 02:13:01 +0000

    update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,7 +18,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
             Generate a random key store the input
             data in Redis using the random key
