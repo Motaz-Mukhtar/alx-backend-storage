2e255ba5cc1d7f7b3724e0662b6055b699cbcbc2
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-21 16:20:08 -0700

    Apped get(), get_str() and get_int() methods to the Cache Class.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -27,3 +27,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]=None) -> Union[str, bytes, int, float]:
+        """
+             take a key string argument and an Optional
+             Callable argument named fn, and convert
+             the data back to the desired format.
+        """
+        data = self._redis.get(key)
+
+        if not data:
+            return None
+        
+        return fn(data)
+        
+
+    def get_str(self, key: str) -> str:
+        """
+            Convert the data to string.
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+            Convert the data to integer.
+        """
+        return self.get(key, int)
